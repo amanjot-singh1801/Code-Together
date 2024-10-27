043e4b346c11debeb00e3ff70da8a0b68e39ffde
amanjot-singh1801 <amanjotsingh1801@gmail.com>
2024-10-27 10:43:01 +0530

    Database changes done

--- Server/config/database.js
+++ Server/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 exports.connect = () =>{
-    mongoose.connect(process.env.MongoDB_URL)
+    mongoose.connect(process.env.MONGODB_URL)
     .then(() => console.log("Database Connected Successfully"))
     .catch( (error) =>{
         console.log("DB connection Failed");
